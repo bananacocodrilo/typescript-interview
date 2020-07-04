c2dc5fc9542e342191321467edbe70adacd610a3
bananacocodrilo <antonio.mkp@gmail.com>
2020-07-04 11:34:21 +0200

    ✨ Add root routes file

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import * as express from "express";
+
+export const register = (app: express.Application) => {
+  // First register the different routes
+  // Also here would be a good place for `/`, `/login`, `/register` ...
+};
