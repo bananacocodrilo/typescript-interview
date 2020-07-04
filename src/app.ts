41765e579114b7528cf13bb99491074596a41201
bananacocodrilo <antonio.mkp@gmail.com>
2020-07-04 11:00:51 +0200

    ✨ Init Express server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,10 @@
 import express from "express";
 
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.info(`server started at http://localhost:${port}`);
+});
