912c8b79ee50013accaa6a46bf898577482d92df
bananacocodrilo <antonio.mkp@gmail.com>
2020-07-04 11:37:07 +0200

    ⚡️ Add interests routes registration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
+import * as interests from "./interests";
 
 export const register = (app: express.Application) => {
   // First register the different routes
+  interests.register(app);
+
   // Also here would be a good place for `/`, `/login`, `/register` ...
 };
